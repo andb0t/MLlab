8985b58db34c161c831b148e855068c8e9072758
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-02-17 00:55:26 +0100

    Implement training for decision stump

--- mllab/src/main/scala/entry/DecisionTree.scala
+++ mllab/src/main/scala/entry/DecisionTree.scala
@@ -10,9 +10,49 @@ class DecisionTreeClassifier(depth: Int = 3) {
 
   def train(X: List[List[Float]], y: List[Int]): Unit = {
     assert (X.length == y.length)
-    decisionTree.addNode(0, 0, 0, true)
-    decisionTree.addNode(1, 1, 0, true)
-    decisionTree.addNode(2, 1, 0, false)
+
+    val nFeatures: Int = X(0).length
+    val nSteps = 10
+
+    def getPurity(x: List[Float], y: List[Int], threshold: Double): (Double, Boolean) = {
+      var gtIdx = x.zip(y).filter(tup => tup._1 > threshold).map(tup => tup._2)
+      var ltIdx = x.zip(y).filter(tup => tup._1 <= threshold).map(tup => tup._2)
+      var TP = gtIdx.filter(_ == 1).length
+      var FP = gtIdx.filter(_ == 0).length
+      var TN = ltIdx.filter(_ == 0).length
+      var FN = ltIdx.filter(_ == 1).length
+      val greater = (TP > FN)
+      val purity: Double = 1.0 * (TP + TN) / x.length
+      // println("threshold " + threshold + " purity " + purity)
+      return Tuple2(purity, greater)
+    }
+
+    def setOptimalCut(nodeIndex: Int): Unit = {
+      for (iFeature <- 0 until nFeatures){
+        var x = X.map(_.apply(iFeature))
+        // println(x.take(10))
+        // println(y.take(10))
+
+        var max = x.max
+        var min  = x.min
+        var stepSize = (max - min) / (nSteps - 1)
+        for (i <- 0 until nSteps) {
+          val currThresh: Double = min + i * stepSize
+          // var currPurity: Double = Double.MinValue
+          // var currGreater: Boolean = true
+          var (currPurity, currGreater) = getPurity(x, y, currThresh)
+          decisionTree.updateNode(nodeIndex, iFeature, currThresh, currGreater, currPurity)
+        }
+      }
+    }
+
+    for (nodeIndex <- 0 until decisionTree.nodes){
+      setOptimalCut(nodeIndex)
+    }
+
+    // decisionTree.addNode(0, 0, 0, true)
+    // decisionTree.addNode(1, 1, 0, true)
+    // decisionTree.addNode(2, 1, 0, false)
     decisionTree.print()
   }
 
@@ -34,6 +74,7 @@ class DecisionNode(){
   var right: Int = -1
   var left: Int = -1
   var parent: Int = -1
+  var purity: Double = Double.MinValue
 }
 
 class DecisionTree(depth: Int){
@@ -44,12 +85,23 @@ class DecisionTree(depth: Int){
     tree += new DecisionNode()
   }
 
-  def addNode(nodeIndex: Int, featureIndex: Int, threshold: Double, greater: Boolean): Unit = {
+  def updateNode(nodeIndex: Int, featureIndex: Int, threshold: Double, greater: Boolean, purity: Double): Unit = {
+    if (tree(nodeIndex).purity >= purity){
+      return
+    }
+    println("Improving purity of node " + nodeIndex +
+      " with feature " + featureIndex +
+      (if (greater) " > " else " < ") + threshold +
+      ": " + tree(nodeIndex).purity +
+      " -> " + purity)
+    addNode(nodeIndex, featureIndex, threshold, greater, purity)
+  }
+
+  def addNode(nodeIndex: Int, featureIndex: Int, threshold: Double, greater: Boolean, purity: Double=Double.MinValue): Unit = {
     if (nodeIndex > tree.length - 1) {
       println("Warning: tree not deep enough! (" + nodeIndex + " > " + (tree.length - 1) + ") Ignore node.")
       return
     }
-    if (tree(nodeIndex).filled) println("Warning: overwriting decision tree node " + nodeIndex)
     tree(nodeIndex).nodeIndex = nodeIndex
     tree(nodeIndex).featureIndex = featureIndex
     tree(nodeIndex).threshold = threshold
@@ -58,6 +110,7 @@ class DecisionTree(depth: Int){
     tree(nodeIndex).right = if ((nodeIndex + 1) * 2 < tree.length) (nodeIndex + 1) * 2 else -1
     tree(nodeIndex).left = if ((nodeIndex + 1) * 2 - 1 < tree.length) (nodeIndex + 1) * 2 - 1 else -1
     tree(nodeIndex).parent = if (nodeIndex == 0) -1 else (nodeIndex - 1) / 2
+    tree(nodeIndex).purity = purity
   }
 
   def isComplete(): Boolean = {
@@ -78,8 +131,8 @@ class DecisionTree(depth: Int){
     println("Tree complete? " + isComplete() + " with " + nodes + " nodes")
     for (node <- tree) {
       println("Node " + node.nodeIndex +
-        ", decides on " + node.featureIndex +
-        " signal " + (if (node.greater) "> " else "< ") + node.threshold +
+        ", decides on feature " + node.featureIndex +
+        (if (node.greater) "> " else "< ") + node.threshold +
         ", parent " + node.parent +" left child " + node.left + " right child " + node.right)
     }
     println("------------------------------")
