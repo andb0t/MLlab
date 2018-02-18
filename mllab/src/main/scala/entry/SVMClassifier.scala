1dfca8abb7e8e3c9e634dfa40d0cb246f1d511b2
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-02-18 11:27:17 -0700

    Implement linear SVM

--- mllab/src/main/scala/entry/SVMClassifier.scala
+++ mllab/src/main/scala/entry/SVMClassifier.scala
@@ -1,9 +1,10 @@
 package mllab
 
 import scala.collection.mutable.ListBuffer
+import scala.util.control.Breaks._
 
 
-class SVMClassifier() {
+class SVMClassifier(alpha: Double = 1) {
 
   var weight = new ListBuffer[Double]()
   var bias: Double = 0
@@ -17,19 +18,78 @@ class SVMClassifier() {
     return prod
   }
 
+  def abs(a: List[Double]): Double = {
+    Math.sqrt(dot(a, a))
+  }
+
+  def getDistance(instance: List[Float]): Double = {
+    var side: Double = bias + dot(weight.toList, instance.map(_.toDouble))
+    side = side / abs(weight.toList)
+    return side
+  }
+
+  def getPrediction(instance: List[Float]): Int = {
+    val prediction = if (getDistance(instance) > 0) 1 else 0
+    return prediction
+  }
+
+  def isCorrect(instance: List[Float], label: Int): Boolean = {
+    if (getPrediction(instance) == label){
+      return true
+    }
+    return false
+  }
+
   def train(X: List[List[Float]], y: List[Int]): Unit = {
     assert (X.length == y.length)
-    weight += 1
-    weight += 0
-    bias = 0
+    // initialize parameters
+    bias  = 0
+    for (i <- 0 until X(0).length) {
+      weight += 0  // TODO: replace by Math.random
+    }
+
+    // loop over training set until all correctly classified
+    var needMoreTraining: Boolean = true
+    var epoch: Int = 0
+    val MaxEpochs: Int = 1000
+    breakable{
+      while(needMoreTraining) {
+        if (epoch > MaxEpochs) {
+          println("Maximum number of loops " + MaxEpochs + " reached! Abort training!")
+          break
+        }
+        println(epoch + ". new training loop with weights " + weight + " and bias " + bias)
+        assert (weight.length == X(0).length)
+        epoch += 1
+        needMoreTraining = false
+        breakable{
+          for (idx <- 0 until X.length) {
+            // take random instance to avoid being trapped in two close instances
+            val i: Int = (Math.random * X.length).toInt
+            if (!isCorrect(X(i), y(i))) {
+              println("Incorrect classification of instance " + i + ": " + X(i) + " true label " + y(i))
+              val sign: Int = if (y(i) == 1) 1 else -1
+              bias = bias + (alpha * sign) / epoch
+              for (j <- 0 until weight.length) {
+                weight(j) = weight(j) + (alpha * sign * X(i)(j)) / epoch
+              }
+              needMoreTraining = true
+              break
+            }
+          }
+        }
+      }
+    }
+    println("Training finished after " + epoch + " epochs!")
+    println("Weight: " + weight)
+    println("Bias: " + bias)
   }
 
   def predict(X: List[List[Float]]): List[Int] = {
     println("This is still a placeholder")
     var result = new ListBuffer[Float]()
     for (instance <- X){
-      val side = bias + dot(weight.toList, instance.map(_.toDouble))
-      val prediction = if (side > 0) 1 else 0
+      val prediction = getPrediction(instance)
       result += prediction
     }
     result.toList.map(_.toInt)
