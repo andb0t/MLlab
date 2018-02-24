8d0eece889c80d867e22a386969def19b3c4113f
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-02-24 16:50:12 -0700

    Remove variable fields

--- src/main/scala/mllab/classifiers/Evaluation.scala
+++ src/main/scala/mllab/classifiers/Evaluation.scala
@@ -7,64 +7,28 @@ object Evaluation{
 
   def precision(y_pred: List[Int], y_true: List[Int]): Double = {
     require(y_pred.length == y_true.length, "both arguments must have the same length")
-    var truePositives: Int = 0
-    var falsePositives: Int = 0
-    for (i <- 0 until y_pred.length) {
-      if (y_true(i) == 1 && y_pred(i) == 1){
-        truePositives += 1
-      }
-      if (y_true(i) == 0 && y_pred(i) == 1){
-        falsePositives += 1
-      }
-    }
+    val truePositives = (y_pred zip y_true) count (_ == (1, 1))
+    val falsePositives = (y_pred zip y_true) count (_ == (1, 0))
     // println("Precision calculation: " + truePositives + " TP and " + falsePositives + " FP" )
-    var precision: Double = Double.NaN
-    if (truePositives + falsePositives != 0) {
-      precision = 1.0 * truePositives / (truePositives + falsePositives)
-    }
-    precision
+    if (truePositives + falsePositives != 0) 1.0 * truePositives / (truePositives + falsePositives)
+    else Double.NaN
   }
 
   def recall(y_pred: List[Int], y_true: List[Int]): Double = {
     require(y_pred.length == y_true.length, "both arguments must have the same length")
-    var truePositives: Int = 0
-    var falseNegatives: Int = 0
-    for (i <- 0 until y_pred.length) {
-      if (y_true(i) == 1 && y_pred(i) == 1){
-        truePositives += 1
-      }
-      if (y_true(i) == 1 && y_pred(i) == 0){
-        falseNegatives += 1
-      }
-    }
+    val truePositives = (y_pred zip y_true) count (_ == (1, 1))
+    val falseNegatives = (y_pred zip y_true) count (_ == (0, 1))
     // println("Recall calculation: " + truePositives + " TP and " + falseNegatives + " FN" )
-    var recall: Double = Double.MaxValue
-    if (truePositives + falseNegatives != 0) {
-      recall = 1.0 * truePositives / (truePositives + falseNegatives)
-    }
-    recall
+    if (truePositives + falseNegatives != 0) 1.0 * truePositives / (truePositives + falseNegatives)
+    else Double.MaxValue
   }
 
   def matrix(y_pred: List[Int], y_true: List[Int]): Unit = {
     require(y_pred.length == y_true.length, "both arguments must have the same length")
-    var truePositives: Int = 0
-    var falsePositives: Int = 0
-    var trueNegatives: Int = 0
-    var falseNegatives: Int = 0
-    for (i <- 0 until y_pred.length) {
-      if (y_true(i) == 1 && y_pred(i) == 1){
-        truePositives += 1
-      }
-      if (y_true(i) == 0 && y_pred(i) == 1){
-        falsePositives += 1
-      }
-      if (y_true(i) == 0 && y_pred(i) == 0){
-        trueNegatives += 1
-      }
-      if (y_true(i) == 1 && y_pred(i) == 0){
-        falseNegatives += 1
-      }
-    }
+    val truePositives = (y_pred zip y_true) count (_ == (1, 1))
+    val falsePositives = (y_pred zip y_true) count (_ == (1, 0))
+    val trueNegatives = (y_pred zip y_true) count (_ == (0, 0))
+    val falseNegatives = (y_pred zip y_true) count (_ == (0, 1))
     println("  P    N")
     println("T " + truePositives + " " + trueNegatives)
     println("F " + falsePositives + " " + falseNegatives)
