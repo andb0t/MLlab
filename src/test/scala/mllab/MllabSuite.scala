7d260c3c1235c29ab07b2457a9f55ded5bdfba1c
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-02-24 16:44:22 -0700

    Add some tests

--- src/test/scala/mllab/MllabSuite.scala
+++ src/test/scala/mllab/MllabSuite.scala
@@ -10,14 +10,18 @@ class MllabSuite extends FunSuite {
 
   test("precision"){
     assert (Evaluation.precision(List(1, 1), List(1, 0)) === 0.5)
+    assert (Evaluation.precision(List(1, 1, 1, 0, 0, 1, 0, 1), List(1, 0, 1, 0, 1, 1, 0, 0)) === 0.6)
   }
 
   test("recall"){
     assert (Evaluation.recall(List(1, 0), List(1, 1)) === 0.5)
+    assert (Evaluation.recall(List(1, 1), List(1, 0)) === 1)
+    assert (Evaluation.recall(List(1, 1, 1, 0, 0, 1, 0, 1), List(1, 0, 1, 0, 1, 1, 0, 0)) === 0.75)
   }
 
   test("f1"){
-    assert (Evaluation.precision(List(1, 1), List(1, 0)) === 0.5)
+    assert (Evaluation.f1(List(1, 1), List(1, 0)) === Math.sqrt(0.5))
+    assert (Evaluation.f1(List(1, 1, 1, 0, 0, 1, 0, 1), List(1, 0, 1, 0, 1, 1, 0, 0)) === Math.sqrt(0.6*0.75))
   }
 
   test("dot product"){
