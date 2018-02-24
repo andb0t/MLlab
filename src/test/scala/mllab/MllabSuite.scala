ee2c1a915ff6f82ca4198a11eed77f3ef60028dd
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-02-24 01:28:26 -0700

    Add test

--- src/test/scala/mllab/MllabSuite.scala
+++ src/test/scala/mllab/MllabSuite.scala
@@ -22,10 +22,12 @@ class MllabSuite extends FunSuite {
 
   test("dot product"){
     assert (Maths.dot(List(1, 2, -1), List(0, -1, -1)) === -1)
+    assert (Maths.dot(List(0, 3), List(1, -1)) === -3)
   }
 
   test("absolute value"){
     assert (Maths.abs(List(1, 2, -1)) === Math.sqrt(6))
+    assert (Maths.abs(List(0, 0)) === 0)
   }
 
 }
