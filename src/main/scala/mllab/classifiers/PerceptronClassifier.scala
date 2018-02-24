969387b9cb93488f4d07101fe6f419c7a84c4d67
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-02-24 16:56:10 -0700

    Shorten methods

--- src/main/scala/mllab/classifiers/PerceptronClassifier.scala
+++ src/main/scala/mllab/classifiers/PerceptronClassifier.scala
@@ -16,13 +16,11 @@ class PerceptronClassifier(alpha: Double = 1) extends Classifier {
     side / Maths.abs(weight.toList)
   }
 
-  def getPrediction(instance: List[Float]): Int = {
+  def getPrediction(instance: List[Float]): Int =
     if (getDistance(instance) > 0) 1 else 0
-  }
 
-  def isCorrect(instance: List[Float], label: Int): Boolean = {
+  def isCorrect(instance: List[Float], label: Int): Boolean =
     getPrediction(instance) == label
-  }
 
   def train(X: List[List[Float]], y: List[Int]): Unit = {
     require(X.length == y.length, "both arguments must have the same length")
@@ -30,7 +28,7 @@ class PerceptronClassifier(alpha: Double = 1) extends Classifier {
     // initialize parameters
     bias  = 0
     for (i <- 0 until X.head.length) {
-      weight += 0  // TODO: replace by Math.random
+      weight += 0  // replace by Math.random?
     }
 
     // loop over training set until all correctly classified
@@ -70,14 +68,7 @@ class PerceptronClassifier(alpha: Double = 1) extends Classifier {
     println("Bias: " + bias)
   }
 
-  def predict(X: List[List[Float]]): List[Int] = {
-    println("This is still a placeholder")
-    var result = new ListBuffer[Float]()
-    for (instance <- X){
-      val prediction = getPrediction(instance)
-      result += prediction
-    }
-    result.toList.map(_.toInt)
-  }
+  def predict(X: List[List[Float]]): List[Int] =
+    for (instance <- X) yield getPrediction(instance)
 
 }
