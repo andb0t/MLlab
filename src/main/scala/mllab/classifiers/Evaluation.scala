cc524c38e40eb4b17725f46b43cc21dc4df41856
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-02-24 01:35:59 -0700

    Simplify function

--- src/main/scala/mllab/classifiers/Evaluation.scala
+++ src/main/scala/mllab/classifiers/Evaluation.scala
@@ -2,10 +2,8 @@ package classifiers
 
 object Evaluation{
 
-  def f1(y_pred: List[Int], y_true: List[Int]): Double = {
-    val f1: Double = Math.sqrt(precision(y_pred, y_true) * recall(y_pred, y_true))
-    f1
-  }
+  def f1(y_pred: List[Int], y_true: List[Int]): Double =
+    Math.sqrt(precision(y_pred, y_true) * recall(y_pred, y_true))
 
   def precision(y_pred: List[Int], y_true: List[Int]): Double = {
     require(y_pred.length == y_true.length, "both arguments must have the same length")
