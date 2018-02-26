fa050bab3a3ecf4b711d98435ef2116cef433596
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-02-26 09:18:40 +0100

    Add predition

--- src/main/scala/mllab/regressors/LinearRegressor.scala
+++ src/main/scala/mllab/regressors/LinearRegressor.scala
@@ -2,13 +2,22 @@ package regressors
 
 import scala.collection.mutable.ListBuffer
 
+import utils._
+
 
 class LinearRegressor() extends Regressor {
 
-  def train(X: List[List[Float]], y: List[Float]): Unit =
+  var weight = new ListBuffer[Double]()
+  var bias: Double = 0
+
+  def train(X: List[List[Float]], y: List[Float]): Unit = {
     require(X.length == y.length, "both arguments must have the same length")
+    for (i <- 0 until X.head.length)
+      weight += 1
+    bias = 0
+  }
 
   def predict(X: List[List[Float]]): List[Float] =
-    for (instance <- X) yield Math.random.toFloat
+    (for (instance <- X) yield Maths.dot(weight.toList, instance.map(_.toDouble)) + bias).map(_.toFloat)
 
 }
