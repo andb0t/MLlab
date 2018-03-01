97553fd4e4601d4b49d83af84904504d8791c4ab
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-03-01 21:48:59 +0100

    Remove custom linalg functions

--- src/main/scala/mllab/utils/Maths.scala
+++ src/main/scala/mllab/utils/Maths.scala
@@ -13,49 +13,6 @@ object Maths{
     (a zip b).map{case (x, y) => x + y}
   }
 
-  def plusM(a: List[List[Double]], b: List[List[Double]]): List[List[Double]] = {
-    require(a.length == b.length, "both arguments must have the same length")
-    (a zip b).map{case (x, y) => plus(x, y)}
-  }
-
-  def times(a: Double, b: List[Double]): List[Double] = {
-    b.map(x => a * x)
-  }
-
-  def timesM(a: Double, b: List[List[Double]]): List[List[Double]] = {
-    b.map(x => times(a, x))
-  }
-
-  def timesMM(a: List[List[Double]], b: List[List[Double]]): List[List[Double]] = {
-    require(a.head.length == b.length, "matrix(" + a.length + ", " + a.head.length +") and vector(" + b.length + ") dimensions do not fit!")
-    (for (bm <- b.transpose) yield a.map(x => dot(x, bm))).transpose
-  }
-
-  def timesMV(a: List[List[Double]], b: List[Double]): List[Double] = {
-    require(a.head.length == b.length, "matrix(" + a.length + ", " + a.head.length +") and vector(" + b.length + ") dimensions do not fit!")
-    a.map(x => dot(x, b))
-  }
-
-  def timesVM(a: List[Double], b: List[List[Double]]): List[Double] = {
-    require(a.length == b.length, "vector(" + a.length +") and matrix(" + b.length + ", " + b.head.length + ") dimensions do not fit!")
-    timesMM(List(a), b).flatten
-  }
-
-  def squareM(M: List[List[Double]]): List[List[Double]] =
-    for (v <- M) yield for (e <- v) yield Math.pow(e, 2)
-
-  def hadamard(a: List[Double], b: List[Double]): List[Double] = {
-    require(a.length == b.length, "both arguments must have the same length")
-    (a zip b).map{case (x, y) => x * y}
-  }
-
-  def hadamardMM(a: List[List[Double]], b: List[List[Double]]): List[List[Double]] = {
-    require(a.length == b.length && a.head.length == b.head.length,
-      "matrix(" + a.length + ", " + a.head.length +") and " +
-      "matrix(" + b.length + ", " + b.head.length + ") dimensions do not fit!")
-    (a zip b).map{case (x, y) => hadamard(x, y)}
-  }
-
   def abs(a: List[Double]): Double = {
     Math.sqrt(dot(a, a))
   }
--- src/test/scala/mllab/UtilsSuite.scala
+++ src/test/scala/mllab/UtilsSuite.scala
@@ -15,51 +15,6 @@ class UtilsSuite extends FunSuite {
     assert (Maths.plus(List(0, 3), List(1, -1)) === List(1, 2))
   }
 
-  test("matrix addition"){
-    val m0: List[List[Double]] = List(List(1, 2, 3), List(4, 5, 6))
-    val m1: List[List[Double]] = List(List(7, 8, 9), List(-3, -2, -1))
-    val m2: List[List[Double]] = List(List(8, 10, 12), List(1, 3, 5))
-    assert (Maths.plusM(m0, m1) === m2)
-  }
-
-  test("vector scaling"){
-    assert (Maths.times(3, List(1, 2, -1)) === List(3, 6, -3))
-  }
-
-  test("matrix scaling"){
-    val m0: List[List[Double]] = List(List(1, 2, 3), List(4, -5, 6))
-    val m1: List[List[Double]] = List(List(3, 6, 9), List(12, -15, 18))
-    assert (Maths.timesM(3, m0) === m1)
-  }
-
-  test("matrix vector multiplication"){
-    val m0: List[List[Double]] = List(List(1, 2, 3), List(4, -5, 6))
-    assert (Maths.timesMV(m0, List(1, 2, -1)) === List(2, -12))
-  }
-
-  test("vector matrix multiplication"){
-    val m0: List[List[Double]] = List(List(1, 2), List(-3, 4), List(-5, 6))
-    assert (Maths.timesVM(List(1, 2, -1), m0) === List(0, 4))
-  }
-
-  test("matrix multiplication"){
-    val m0: List[List[Double]] = List(List(1, 2, 3), List(4, -5, 6))
-    val m1: List[List[Double]] = List(List(2, 1), List(2, -1), List(1, 0))
-    val m2: List[List[Double]] = List(List(9, -1), List(4, 9))
-    assert (Maths.timesMM(m0, m1) === m2)
-  }
-
-  test("hadamard product"){
-    assert (Maths.hadamard(List(1, 2, -1), List(0, -1, -1)) === List(0, -2, 1))
-  }
-
-  test("hadamard matrix product"){
-    val m0: List[List[Double]] = List(List(1, 2, 3), List(4, -5, 6))
-    val m1: List[List[Double]] = List(List(2, 1, 2), List(-1, 1, 0))
-    val m2: List[List[Double]] = List(List(2, 2, 6), List(-4, -5, 0))
-    assert (Maths.hadamardMM(m0, m1) === m2)
-  }
-
   test("absolute value"){
     assert (Maths.abs(List(1, 2, -1)) === Math.sqrt(6))
     assert (Maths.abs(List(0, 0)) === 0)
