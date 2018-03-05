d9a33ca25413215f8f422445b2786e747e093f7e
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-03-05 13:26:50 +0100

    Unify coloring

--- src/main/scala/mllab/plotting/plot.scala
+++ src/main/scala/mllab/plotting/plot.scala
@@ -12,7 +12,7 @@ object Plotting {
     val f = Figure()
     f.visible= false
     val p = f.subplot(0)
-    for (category: Int <- labels.toSet) {
+    for (category: Int <- labels.toSet.toList.sorted) {
       val filteredData: List[List[Double]] = (data zip labels).filter(_._2 == category).map(_._1)
       val x: List[Double] = filteredData.map(e => e.head)
       val y: List[Double] = filteredData.map(e => e(1))
@@ -31,7 +31,7 @@ object Plotting {
     f.visible= false
     val p = f.subplot(0)
     // now plot all datapoints
-    for (category: Int <- predictions.toSet) {
+    for (category: Int <- predictions.toSet.toList.sorted) {
       val filteredData: List[List[Double]] = (data zip predictions).filter(_._2 == category).map(_._1)
       val x: List[Double] = filteredData.map(e => e.head)
       val y: List[Double] = filteredData.map(e => e(1))
@@ -76,7 +76,7 @@ object Plotting {
     val f = Figure()
     f.visible= false
     val p = f.subplot(0)
-    for (category: Int <- predictions.toSet) {
+    for (category: Int <- predictions.toSet.toList.sorted) {
       val filteredData: List[List[Double]] = (gridData zip predictions).filter(_._2 == category).map(_._1)
       val x: List[Double] = filteredData.map(e => e.head)
       val y: List[Double] = filteredData.map(e => e(1))
