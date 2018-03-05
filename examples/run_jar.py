2e6f0cef9b7a042239799afb301c7ebee2624023
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-03-05 16:02:30 +0100

    Add arguments to jar call

--- examples/run_jar.py
+++ examples/run_jar.py
@@ -1,4 +1,8 @@
 import subprocess
 
 
-subprocess.call(['java', '-jar', 'target/scala-2.11/mllab-assembly-0.1.0-SNAPSHOT.jar'])
+subprocess.call(['java',
+                 '-jar',
+                 'target/scala-2.11/mllab-assembly-0.1.0-SNAPSHOT.jar',
+                 'reg',
+                 'Linear'])
