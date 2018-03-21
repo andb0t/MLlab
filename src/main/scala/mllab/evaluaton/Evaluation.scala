afda63a4892386fe0e2ac43ec87d2e50235675d9
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-03-21 16:41:47 +0100

    Document evaluation

--- src/main/scala/mllab/evaluaton/Evaluation.scala
+++ src/main/scala/mllab/evaluaton/Evaluation.scala
@@ -1,10 +1,20 @@
 package evaluation
 
+
+/** Provides functions for evaluation of algorithm performance */
 object Evaluation{
 
+  /** Calculates the F1 score
+   *@param y_pred List of predictions
+   *@param y_true List of labels
+   */
   def f1(y_pred: List[Int], y_true: List[Int]): Double =
     Math.sqrt(precision(y_pred, y_true) * recall(y_pred, y_true))
 
+  /** Calculates the precision score
+   *@param y_pred List of predictions
+   *@param y_true List of labels
+   */
   def precision(y_pred: List[Int], y_true: List[Int]): Double = {
     require(y_pred.length == y_true.length, "both arguments must have the same length")
     val truePositives = (y_pred zip y_true) count (_ == (1, 1))
@@ -14,6 +24,10 @@ object Evaluation{
     else Double.NaN
   }
 
+  /** Calculates the recall score
+   *@param y_pred List of predictions
+   *@param y_true List of labels
+   */
   def recall(y_pred: List[Int], y_true: List[Int]): Double = {
     require(y_pred.length == y_true.length, "both arguments must have the same length")
     val truePositives = (y_pred zip y_true) count (_ == (1, 1))
@@ -23,6 +37,10 @@ object Evaluation{
     else Double.MaxValue
   }
 
+  /** Prints the confusion matrix
+   *@param y_pred List of predictions
+   *@param y_true List of labels
+   */
   def matrix(y_pred: List[Int], y_true: List[Int]): Unit = {
     require(y_pred.length == y_true.length, "both arguments must have the same length")
     val truePositives = (y_pred zip y_true) count (_ == (1, 1))
@@ -34,9 +52,17 @@ object Evaluation{
     println("F " + falsePositives + " " + falseNegatives)
   }
 
+  /** Calculates the Mean Squared Error
+   *@param y_pred List of predictions
+   *@param y_true List of labels
+   */
   def MSE(y_pred: List[Double], y_true: List[Double]): Double =
     (y_pred zip y_true).map{case (x, y) => Math.pow(x - y, 2)}.sum / y_true.length
 
+  /** Calculates the log loss
+   *@param p_pred List of probabilities for each instance
+   *@param y_true List of labels
+   */
   def LogLoss(p_pred: List[Double], y_true: List[Int]): Double =
     - (p_pred zip y_true).map{case (p, y) => y * Math.log(p) + (1 - y) * Math.log(1 - p)}.sum / y_true.length
 
