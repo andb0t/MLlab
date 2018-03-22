b649000e9b8751f7394e07d54a7fffbd6217a939
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-03-22 17:59:45 +0100

    Generalize to any number of features and polynomial order

--- src/main/scala/mllab/datastructures/PolyFeatures.scala
+++ src/main/scala/mllab/datastructures/PolyFeatures.scala
@@ -2,35 +2,36 @@ package datastructures
 
 
 object DataTrafo {
-  def addPolyFeatures(X: List[List[Double]], degree: Int): List[List[Double]] = {
-    if (degree == 1) X
+
+  /** Produces a list of all possible multiplicities of at least 2 features */
+  def polyList(deg: Int, nFeat: Int, list: List[List[Int]]): List[List[Int]] = {
+    def oneHot(idx: Int): List[Int] =
+      (for (i <- 0 until nFeat) yield if (i == idx) 1 else 0).toList
+    def addOne(list: List[Int]): List[List[Int]] = list match {
+        case Nil => (for (i <- 0 until nFeat) yield oneHot(i)).toList
+        case _ =>
+          (for (i <- 0 until nFeat) yield list.zip(oneHot(i)).map{case (x, y) => x + y}).toList
+      }
+    if (deg == 0) list
+    else {
+      val thisDegreeList =
+        if (list == Nil) (for (i <- 0 until nFeat) yield oneHot(i)).toList
+        else if (deg == 1) (list.map(l => addOne(l))).flatten  // don't record unit multiplicities
+        else (list.map(l => addOne(l))).flatten ::: list
+      polyList(deg-1, nFeat, thisDegreeList.toSet.toList)
+    }
+  }
+
+    /** Turns turns the lists with multiplicities into polynomial mapping */
+  def polyMap(order: Int, nFeatures: Int): List[Map[Int, Int]] =
+    polyList(order, nFeatures, Nil).map(l => (for (i <- 0 until l.length) yield i -> l(i)).toMap)
+
+  /** Adds polynomial features of up until given order */
+  def addPolyFeatures(X: List[List[Double]], order: Int): List[List[Double]] = {
+    if (order == 1) X
     else {
       val xFeatures = X.transpose
       val nFeatures = xFeatures.length
-
-      def polyMap(deg: Int): List[Map[Int, Int]] = {
-
-        // def orderMaps(deg: Int, nFeat: Int, list: List[Int], thisDeg: Int = 2): List[Int] = {
-        //   def addOne(list: List[Int]): List[List[Int]] =
-        //       for (i <- 0 until list.length) yield
-        //       list.map(l =>)
-        //
-        //   if (deg < 2) list
-        //   else {
-        //     val thisDegreeList =
-        //     orderMaps(deg-1, nFeat, thisDegreeList)
-        //   }
-        // }
-        //
-        // val resultMaps = orderMaps(degree, nFeatures, List.fill(nFeatures)(0))
-
-        val maps = for (i <- 2 to degree) yield for (j <- 0 until nFeatures) yield Map(j -> i)
-        val finalMap: List[Map[Int, Int]] = maps.flatten.toSet.toList
-
-        println("Final polymap (feature -> order):")
-        println(finalMap.mkString("\n"))
-        finalMap
-      }
 
       def addFeatures(xExtended: List[List[Double]], featureMapList: List[Map[Int, Int]]): List[List[Double]] = featureMapList match {
         case Nil => xExtended
@@ -40,9 +41,11 @@ object DataTrafo {
           addFeatures(newFeature::xExtended, mTail)
         }
       }
-
-      val xExtended = addFeatures(xFeatures, polyMap(degree)).transpose
-      // println("Instances with the transformed features:")
+      val polyMaps = polyMap(order, nFeatures)
+      // println("Polynomial mapping (%d maps):".format(polyMaps.length))
+      // println(polyMaps.sortBy(_.values).mkString("\n"))
+      val xExtended = addFeatures(xFeatures, polyMaps).transpose
+      println("Instances with added polynomial features (%d -> %d features)".format(X.head.length, xExtended.head.length))
       // println(X.head + " -> " + xExtended.head)
       // println(X(1) + " -> " + xExtended(1))
       // println(X(2) + " -> " + xExtended(2))
