c0755a68289ba6bd8385ad5caf2f1c09e7a5f93c
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-03-22 13:50:43 +0100

    Add polynomial features

--- src/main/scala/mllab/regressors/LinearRegressor.scala
+++ src/main/scala/mllab/regressors/LinearRegressor.scala
@@ -6,7 +6,7 @@ import evaluation._
 import utils._
 
 
-class LinearRegressor(alpha: Double = 1, tol: Double = 0.001, maxIter: Int = 1000) extends Regressor {
+class LinearRegressor(alpha: Double = 1, tol: Double = 0.001, maxIter: Int = 1000, degree: Int=1) extends Regressor {
 
   val name: String = "LinearRegressor"
 
@@ -14,6 +14,14 @@ class LinearRegressor(alpha: Double = 1, tol: Double = 0.001, maxIter: Int = 100
   var bias: Double = 0
 
   var lossEvolution = new ListBuffer[(Double, Double)]()
+
+  def addPolyFeatures(X: List[List[Double]], degree: Int=1): List[List[Double]] = {
+    val xFeatures = X.transpose
+    // quick hack for 1 feature
+    degree  // dummy line
+    val xSquaredFeature = for (x <- xFeatures.head) yield Math.pow(x, 2)
+    (xSquaredFeature::xFeatures).transpose
+  }
 
   def lossGradient(X: List[List[Double]], y: List[Double]): List[Double] = {
     // dLoss = d(MSE scaled) = Sum (const * linearDistanceScaled * instanceVector)
@@ -23,14 +31,14 @@ class LinearRegressor(alpha: Double = 1, tol: Double = 0.001, maxIter: Int = 100
     biasGradient::weightGradient
   }
 
-  def train(X: List[List[Double]], y: List[Double]): Unit = {
+  def _train(X: List[List[Double]], y: List[Double]): Unit = {
     require(X.length == y.length, "both arguments must have the same length")
     for (i <- 0 until X.head.length)
       weight += 0
     bias = 0
 
     def updateWeights(count: Int): Unit = {
-      val loss = Evaluation.MSE(predict(X), y)
+      val loss = Evaluation.MSE(_predict(X), y)
       lossEvolution += Tuple2(count.toDouble, loss)
       if (loss > tol && count < maxIter) {
         val weightUpdate = lossGradient(X, y).map(_ * alpha)
@@ -53,8 +61,14 @@ class LinearRegressor(alpha: Double = 1, tol: Double = 0.001, maxIter: Int = 100
     updateWeights(0)
   }
 
-  def predict(X: List[List[Double]]): List[Double] =
+  def _predict(X: List[List[Double]]): List[Double] =
     for (instance <- X) yield Maths.dot(weight.toList, instance) + bias
+
+  def predict(X: List[List[Double]]): List[Double] =
+    _predict(addPolyFeatures(X, degree))
+
+  def train(X: List[List[Double]], y: List[Double]): Unit =
+    _train(addPolyFeatures(X, degree), y)
 
   override def diagnostics(): Map[String, List[(Double, Double)]] = {
     Map("loss" -> lossEvolution.toList)
