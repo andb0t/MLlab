da4eaebdc0e021aedc7b4dcc8f065304f2d0e0c5
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-03-23 12:19:07 +0100

    Test addition of poly features

--- src/test/scala/mllab/DataStructuresSuite.scala
+++ src/test/scala/mllab/DataStructuresSuite.scala
@@ -5,12 +5,12 @@ import datastructures._
 
 class DataStructuresSuite extends FunSuite {
 
-  test("polynomial components") {
+  test ("polynomial components") {
     val twoTwo = List(List(2, 0), List(1, 1), List(0, 2))
-    assert (DataTrafo.polyList(2, 2, Nil).toSet  === twoTwo.toSet)
+    assert (DataTrafo.polyList(2, 2, Nil).toSet === twoTwo.toSet)
     assert (DataTrafo.polyList(2, 2, Nil).length === twoTwo.length)
     val threeTwo = List(List(3, 0), List(2, 1), List(1, 2), List(0, 3), List(2, 0), List(1, 1), List(0, 2))
-    assert (DataTrafo.polyList(3, 2, Nil).toSet  === threeTwo.toSet)
+    assert (DataTrafo.polyList(3, 2, Nil).toSet === threeTwo.toSet)
     assert (DataTrafo.polyList(3, 2, Nil).length === threeTwo.length)
     val threeThree = List(
       List(3, 0, 0), List(0, 3, 0), List(0, 0, 3),
@@ -19,8 +19,13 @@ class DataStructuresSuite extends FunSuite {
       List(0, 1, 1), List(1, 0, 1), List(1, 1, 0),
       List(1, 1, 1)
     )
-    assert (DataTrafo.polyList(3, 3, Nil).toSet  === threeThree.toSet)
+    assert (DataTrafo.polyList(3, 3, Nil).toSet === threeThree.toSet)
     assert (DataTrafo.polyList(3, 3, Nil).length === threeThree.length)
+  }
+
+  test ("add polynomial features") {
+    assert (DataTrafo.addPolyFeatures(List(List(1.0, 2.0)), 2).map(_.sorted) === List(List(1.0, 2.0, 1.0, 2.0, 4.0)).map(_.sorted))
+    assert (DataTrafo.addPolyFeatures(List(List(1.0, 2.0)), 1).map(_.sorted) === List(List(1.0, 2.0)).map(_.sorted))
   }
 
   test ("decision nodes") {
