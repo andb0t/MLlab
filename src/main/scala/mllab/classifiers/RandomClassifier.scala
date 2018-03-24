dd6c499ba40147e896299a1d2bce32f3ee208468
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-03-24 18:06:42 +0100

    Adapt to multiclass output

--- src/main/scala/mllab/classifiers/RandomClassifier.scala
+++ src/main/scala/mllab/classifiers/RandomClassifier.scala
@@ -2,15 +2,22 @@ package classifiers
 
 import scala.collection.mutable.ListBuffer
 
-
+/** Random classifier
+ *
+ * This is a classifier deciding
+ */
 class RandomClassifier() extends Classifier {
 
   val name: String = "RandomClassifier"
 
-  def train(X: List[List[Double]], y: List[Int]): Unit =
+  var nFeatures: Int = 0
+
+  def train(X: List[List[Double]], y: List[Int]): Unit = {
     require(X.length == y.length, "number of training instances and labels is not equal")
+    nFeatures = y.toSet.size
+  }
 
   def predict(X: List[List[Double]]): List[Int] =
-    for (instance <- X) yield if (Math.random < 0.5) 0 else 1
+    for (instance <- X) yield (Math.random * nFeatures).toInt
 
 }
