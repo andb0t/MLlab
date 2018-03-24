b5edac815032f4d2110c801de9fd4fcc1ae93af9
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-03-24 13:08:09 +0100

    Add arguments for priors

--- src/main/scala/mllab/classifiers/NaiveBayesClassifier.scala
+++ src/main/scala/mllab/classifiers/NaiveBayesClassifier.scala
@@ -5,7 +5,7 @@ import scala.collection.mutable.ListBuffer
 import utils._
 
 
-class NaiveBayesClassifier(model: String="gaussian") extends Classifier {
+class NaiveBayesClassifier(model: String="gaussian", priors: List[Double]=Nil) extends Classifier {
 
   val name: String = "SVMClassifier"
 
@@ -13,6 +13,9 @@ class NaiveBayesClassifier(model: String="gaussian") extends Classifier {
   var prior = new ListBuffer[Double]()
   /** Parameters for the likelihood for each class and feature */
   var params = new ListBuffer[List[List[Double]]]()
+
+  if (priors != Nil)
+    assert (priors.sum == 1, "provided priors do not add up to unity")
 
   /** Calculates the per class and per feature likelihood value of a given instance */
   def getLikeli(x: List[Double]): List[List[Double]] =
@@ -32,9 +35,15 @@ class NaiveBayesClassifier(model: String="gaussian") extends Classifier {
   def train(X: List[List[Double]], y: List[Int]): Unit = {
     require(X.length == y.length, "number of training instances and labels is not equal")
     val classes: List[Int] = y.toSet.toList
-    println("Determine prior from training set frequencies")
-    for (cl <- classes) {
-      prior += 1.0 * y.count(_==cl) / y.length
+    if (priors != Nil) {
+      for (p <- priors) prior += p
+      assert (prior.length == classes.length, "number of provided priors does not match number of classes")
+    }
+    else {
+      println("Determine priors from training set frequencies")
+      for (cl <- classes) {
+        prior += 1.0 * y.count(_==cl) / y.length
+      }
     }
     println("Prior:")
     println(prior.zipWithIndex.map{case (p, c) => " - class " + c + ": " + p}.mkString("\n"))
