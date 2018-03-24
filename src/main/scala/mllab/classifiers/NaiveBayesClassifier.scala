14311a9fe4acc0ac919853f5fd482a0944fae0dc
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-03-24 12:26:04 +0100

    Add documentation

--- src/main/scala/mllab/classifiers/NaiveBayesClassifier.scala
+++ src/main/scala/mllab/classifiers/NaiveBayesClassifier.scala
@@ -14,11 +14,13 @@ class NaiveBayesClassifier(model: String="gaussian") extends Classifier {
   /** Parameters for the likelihood for each class and feature */
   var params = new ListBuffer[List[List[Double]]]()
 
+  /** Calculates the per class and per feature likelihood value of a given instance */
   def getLikeli(x: List[Double]): List[List[Double]] =
     if (model == "gaussian")
-      (for (pClass <- params) yield for (ip <- x zip pClass) yield Maths.norm(ip._1, ip._2.head ,ip._2(1))).toList
+      (for (pClass <- params) yield for (fp <- x zip pClass) yield Maths.norm(fp._1, fp._2.head ,fp._2(1))).toList
     else throw new NotImplementedError("Bayesian model " + model + " not implemented")
 
+  /** Calculates the probabilities of belonging to a class for a given instance */
   def getProbabs(x: List[Double]): List[Double] =
     (for (pl <- prior zip getLikeli(x)) yield pl._1 * pl._2.product).toList
 
@@ -46,6 +48,7 @@ class NaiveBayesClassifier(model: String="gaussian") extends Classifier {
 
   def predict(X: List[List[Double]]): List[Int] = {
     val result = for (instance <- X) yield getProbabs(instance).zipWithIndex.maxBy(_._1)._2
+    println("Details:")
     println((X zip X.map(getProbabs(_)) zip result).take(5).mkString("\n"))
     result
   }
--- src/main/scala/mllab/utils/Maths.scala
+++ src/main/scala/mllab/utils/Maths.scala
@@ -21,15 +21,23 @@ object Maths{
     Math.sqrt(dot(a, a))
   }
 
+  /** Normal (Gaussian) function
+   * @param x Abscissa
+   * @param m Mean
+   * @param s Sigma
+   */
   def norm(x: Double, m: Double, s: Double): Double =
     Math.exp( -Math.pow((x - m), 2) / (2 * Math.pow(s, 2))) / Math.sqrt(2 * math.Pi * Math.pow(s, 2))
 
+  /** Returns the mean of a list of values */
   def mean(l: List[Double]): Double =
     l.sum / l.length
 
+    /** Returns the variance of a list of values */
   def variance(l: List[Double]): Double =
       l.map(a => Math.pow(a - mean(l), 2)).sum / l.size
 
+      /** Returns the standard deviation of a list of values */
   def std(l: List[Double]): Double =
     Math.sqrt(variance(l))
 
