ac91581c88b803a555e34b2a9ce821f2b7bab2df
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-03-25 20:04:43 +0200

    Add dummy prediction method

--- src/main/scala/mllab/regressors/DecisionTreeRegressor.scala
+++ src/main/scala/mllab/regressors/DecisionTreeRegressor.scala
@@ -2,6 +2,8 @@ package regressors
 
 import scala.collection.mutable.ListBuffer
 
+import datastructures._
+
 
 /** Decision tree regressor
  *
@@ -11,10 +13,12 @@ class DecisionTreeRegressor(depth: Int = 3) extends Regressor {
 
   val name: String = "DecisionTreeRegressor"
 
+  var decisionTree = new DecisionTree(depth)
+
   def train(X: List[List[Double]], y: List[Double]): Unit =
     require(X.length == y.length, "both arguments must have the same length")
 
   def predict(X: List[List[Double]]): List[Double] =
-    for (instance <- X) yield Math.random
+    for (x <- X) yield decisionTree.predict(x)
 
 }
