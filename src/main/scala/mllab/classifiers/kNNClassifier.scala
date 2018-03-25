771cd3bdadff85eca3242ad9fca72261253afe27
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-03-25 13:01:16 +0200

    Add documentation

--- src/main/scala/mllab/classifiers/kNNClassifier.scala
+++ src/main/scala/mllab/classifiers/kNNClassifier.scala
@@ -22,6 +22,12 @@ class kNNClassifier(k: Int = 3) extends Classifier {
     y.copyToBuffer(y_NN)
   }
 
+  /** Yield new collection of nearest neighbors
+   * @param x Test instance feature vector
+   * @param instance Training instance feature vector
+   * @param label Training instance label
+   * @param nearest Current collection of nearest neighbors
+   */
   def updateNearest(x: List[Double], instance: List[Double], label: Int, nearest: List[Tuple2[Double, Int]]): List[Tuple2[Double, Int]] = {
     val distance: Double = Maths.distance(x, instance)
     val candidate: Tuple2[Double, Int] = (distance, label)
@@ -32,12 +38,14 @@ class kNNClassifier(k: Int = 3) extends Classifier {
       nearest
   }
 
-  def loopEvents(x: List[Double], X_NN: List[List[Double]], y_NN: List[Int], nearest: List[Tuple2[Double, Int]]): List[Tuple2[Double, Int]] =
+  /** Gets list of nearest neighbors */
+  def getNearest(x: List[Double], X_NN: List[List[Double]], y_NN: List[Int], nearest: List[Tuple2[Double, Int]]): List[Tuple2[Double, Int]] =
     if (X_NN == Nil) nearest
-    else loopEvents(x, X_NN.tail, y_NN.tail, updateNearest(x, X_NN.head, y_NN.head, nearest))
+    else getNearest(x, X_NN.tail, y_NN.tail, updateNearest(x, X_NN.head, y_NN.head, nearest))
 
+    /** Predicts a label for a single instance */
   def getPrediction(x: List[Double], classes: List[Int]): Int = {
-    val nearest = loopEvents(x, X_NN.toList, y_NN.toList, List.fill(k)(Tuple2(Double.MaxValue, -1)))
+    val nearest = getNearest(x, X_NN.toList, y_NN.toList, List.fill(k)(Tuple2(Double.MaxValue, -1)))
     val labels = for (dl <- nearest) yield dl._2
     val probab: List[Double] = for (l <- classes.sorted) yield 1.0 * labels.count(_==l) / labels.length
     probab.zipWithIndex.maxBy(_._1)._2
