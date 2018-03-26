3aa429e82d4dffaa1509e2c8bdcd2a414a8b1d00
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-03-26 09:49:52 +0200

    Make more functional

--- src/main/scala/mllab/regressors/DecisionTreeRegressor.scala
+++ src/main/scala/mllab/regressors/DecisionTreeRegressor.scala
@@ -15,12 +15,10 @@ class DecisionTreeRegressor(depth: Int = 6) extends Regressor {
 
   /** Calculates the purity for a cut at threshold at this node
    * @param featureX List of this feature for all instances at this node
-   * @param yThisNode List of the corresponding labels
    * @param threshold The cut-off threshold to be applied
-   * @return The purity of this split and a boolean flat indicating if signal region is greater than the threshold
+   * @return The purity of this split
    */
-  def getPurity(featureX: List[Double], yThisNode: List[Double], threshold: Double): (Double, Boolean) = {
-
+  def getPurity(featureX: List[Double], threshold: Double): Double = {
     val belowX: List[Double] = featureX.filter(_ < threshold)
     val aboveX: List[Double] = featureX.filter(_ > threshold)
     val belowXMean: Double = belowX.sum / belowX.length
@@ -28,7 +26,7 @@ class DecisionTreeRegressor(depth: Int = 6) extends Regressor {
     val cost: Double = Math.sqrt(belowX.map(x => Math.pow(x - belowXMean, 2)).sum + aboveX.map(x => Math.pow(x - aboveXMean, 2)).sum)
     val purity: Double = -cost
     // println("threshold " + threshold + " purity " + purity)
-    Tuple2(purity, true)
+    purity
   }
 
   /** Sets optimal feature and cut-off value for this node
@@ -38,42 +36,40 @@ class DecisionTreeRegressor(depth: Int = 6) extends Regressor {
    * @param nodeIndex Index of the node to tune
    */
   def setOptimalCut(X: List[List[Double]], y: List[Double], decTree: DecisionTree, nodeIndex: Int): Unit = {
-    println("Tuning node " + nodeIndex)
-    val nSteps = 10
-    val nZooms = 3
+    // println("Tuning node " + nodeIndex)
+    val nSteps: Int = 10
+    val nZooms: Int = 3
     val mean: Double = 1.0 * y.sum / y.length
+
+    def zoom(count: Int, min: Double, stepSize: Double, featureX: List[Double], iFeature: Int): Unit =
+      if (count < nZooms) {
+
+        def scanSteps(count: Int, purestSplit: Double, maxPurity: Double): Unit =
+          if (count < nSteps) {
+            val currThresh: Double = min + count * stepSize
+            val currPurity = getPurity(featureX, currThresh)
+            decTree.updateNode(nodeIndex, iFeature, currThresh, true, mean, currPurity)
+            if (maxPurity < currPurity) scanSteps(count+1, currThresh, currPurity)
+            else scanSteps(count+1, purestSplit, maxPurity)
+          }
+
+        scanSteps(0, 0, Double.MinValue)
+        val newMin: Double = -stepSize
+        val newStepSize: Double = 2.0 * stepSize / (nSteps - 1)
+        zoom(count+1, newMin, newStepSize, featureX, iFeature)
+      }
 
     if (!X.isEmpty) {
       val nFeatures: Int = X.head.length
       for (iFeature <- 0 until nFeatures){
-        val featureX = X.map(_.apply(iFeature))
+        val featureX: List[Double] = X.map(_.apply(iFeature))
         val featureMax: Double = featureX.max
         val featureMin: Double = featureX.min
-        val range = featureMax - featureMin
-
-        var max = featureMax + 0.01 * range
-        var min = featureMin - 0.01 * range
-        var stepSize = (max - min) / (nSteps - 1)
-        var purestStep: Double = 0
-        var maxPurity: Double = Double.MinValue
-
-        for (i <- 0 until nZooms) {
-          for (i <- 0 until nSteps) {
-            val currThresh: Double = min + i * stepSize
-            val (currPurity, currGreater) = getPurity(featureX, y, currThresh)
-            decTree.updateNode(nodeIndex, iFeature, currThresh, currGreater, mean, currPurity)
-            if (maxPurity < currPurity) {
-              maxPurity = currPurity
-              purestStep = currThresh
-            }
-          }
-          max = purestStep + stepSize
-          min = purestStep - stepSize
-          stepSize = (max - min) / (nSteps - 1)
-          purestStep = 0
-          maxPurity = Double.MinValue
-        }
-
+        val range: Double = featureMax - featureMin
+        val max: Double = featureMax + 0.01 * range
+        val min: Double = featureMin - 0.01 * range
+        val stepSize: Double = (max - min) / (nSteps - 1)
+        zoom(0, min, stepSize, featureX, iFeature)
       }
     }
   }
