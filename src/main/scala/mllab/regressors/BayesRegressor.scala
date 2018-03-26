671809b53b5ba1e37a838a8fb45491a92a67a6bb
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-03-27 00:43:15 +0200

    Rename variables to mirror theory

--- src/main/scala/mllab/regressors/BayesRegressor.scala
+++ src/main/scala/mllab/regressors/BayesRegressor.scala
@@ -13,7 +13,7 @@ import utils._
  *
  * The priors can be set using the priorPars parameter. If left empty, random values will be used.
  * e.g. List(List(0, 1), List(1, 1), List(2)) for Gaussian priors for the intercept (mean 0, sigma 1) and
- * the weights (mean 1, sigma 1) and with the posterior width 2
+ * the other weights (mean 1, sigma 1) and with the posterior width 2
  *
  * @param degree Order of polynomial features to add to the instances (1 for no addition)
  * @param model The shape of the prior function
@@ -24,69 +24,43 @@ class BayesRegressor(degree: Int=1, model: String= "gaussian", priorPars: List[L
 
   val name: String = "BayesRegressor"
 
-  var paramB: List[Double] = Nil
-  var paramS: Double = 0
+  /** The weight vector to be determined by the training */
+  var weight: List[Double] = Nil
+  /** The posterior width to be determined by the training */
+  var width: Double = 0
 
   // Parameter likelihood assumptions
-  var sigmaLike: Double = 0
-  var meanB: List[Double] = Nil
-  var sigmaB: List[Double] = Nil
-
-  lazy val defaultPars: Map[String, Double] =
-    Map("gaussian" -> 1.0,
-    "rectangular" -> 1.0)
-
-  lazy val randomPars: Map[String, Double] =
-    Map("gaussian" -> (1.0 * scala.util.Random.nextInt(3) + 1),
-    "rectangular" -> (1.0 * scala.util.Random.nextInt(3) + 1))
-
-  lazy val customPars: Map[String, Double] =
-    Map("gaussian" -> priorPars.last.head,
-    "rectangular" -> priorPars.last.head)
+  var widthPrior: Double = 0
+  var weightPrior: List[Double] = Nil
+  var weightPriorWidth: List[Double] = Nil
 
   def priorFunc(model: String, x: Double, params: List[Double]): Double =
     if (model == "gaussian") Maths.normal(x, params.head, params.last)
-    else if (model == "rectangular") Maths.rectangular(x, params.head, params.last)
+    else if (model == "rectangular") Maths.rectangular(x, params.head - params.last, params.head + params.last)
     else throw new NotImplementedError("model " + model + " is not implemented")
 
   // gaussian priors for linear parameters
-  val priorB = (b: List[Double]) => {(b zip (meanB zip sigmaB)).map{case (bi, ms) => priorFunc(model, bi, List(ms._1, ms._2))}}
+  val evalWeightPrior = (b: List[Double]) => {(b zip (weightPrior zip weightPriorWidth)).map{case (bi, ms) => priorFunc(model, bi, List(ms._1, ms._2))}}
   // rectangular prior for prior sigma
-  val priorS = (s: Double) => {Maths.rectangular(s, 0, sigmaLike)}
+  val evalWidthPrior = (s: Double) => {Maths.rectangular(s, 0, widthPrior)}
 
-  def initWeights(model: String, nFeatures: Int): Unit = {
-    if (!priorPars.isEmpty) sigmaLike = customPars(model)
-    else if (randInit) sigmaLike = randomPars(model)
-    else sigmaLike = defaultPars(model)
-
-    if (model == "gaussian")
-      if (priorPars.isEmpty) {
-        if (randInit){
-          meanB = (for (i <- 0 to nFeatures) yield 1.0 * scala.util.Random.nextInt(7) - 3).toList
-          sigmaB = (for (i <- 0 to nFeatures) yield 1.0 * scala.util.Random.nextInt(3) + 1).toList
-        }
-        else {
-          meanB = (for (i <- 0 to nFeatures) yield Math.ceil(1.0 * i / 2) * ((i % 2) * 2 - 1)).toList
-          sigmaB = (for (i <- 0 to nFeatures) yield 1.0 * (i % 4 + 1)).toList
-        }
-      }
-      else {
-        meanB = (for (i <- 0 to nFeatures) yield priorPars(1 + i).head).toList
-        sigmaB = (for (i <- 0 to nFeatures) yield priorPars(1 + i).last).toList
-      }
-    else if (model == "rectangular") {
+  def initWeights(nFeatures: Int): Unit = {
+    if (priorPars.isEmpty) {
       if (randInit){
-        meanB = (for (i <- 0 to nFeatures) yield -1.0 * scala.util.Random.nextInt(7) - 1).toList
-        sigmaB = (for (i <- 0 to nFeatures) yield 1.0 * scala.util.Random.nextInt(7) + 1).toList
+        widthPrior = 1.0 * scala.util.Random.nextInt(3) + 1
+        weightPrior = (for (i <- 0 to nFeatures) yield 1.0 * scala.util.Random.nextInt(7) - 3).toList
+        weightPriorWidth = (for (i <- 0 to nFeatures) yield 1.0 * scala.util.Random.nextInt(3) + 1).toList
       }
       else {
-        meanB = (for (i <- 0 to nFeatures) yield Math.ceil(1.0 * i / 2) * ((i % 2) * 2 - 1)).toList
-        sigmaB = (for (i <- 0 to nFeatures) yield meanB(i) + 5 + 1.0 * (i % 4 + 1)).toList
+        widthPrior = 1.0
+        weightPrior = (for (i <- 0 to nFeatures) yield Math.ceil(1.0 * i / 2) * ((i % 2) * 2 - 1)).toList
+        weightPriorWidth = (for (i <- 0 to nFeatures) yield 1.0 * (i % 4 + 1)).toList
       }
     }
     else {
-      meanB = (for (i <- 0 to nFeatures) yield priorPars(1 + i).head).toList
-      sigmaB = (for (i <- 0 to nFeatures) yield priorPars(1 + i).last).toList
+      widthPrior = priorPars.last.head
+      weightPrior = (for (i <- 0 to nFeatures) yield priorPars(1 + i).head).toList
+      weightPriorWidth = (for (i <- 0 to nFeatures) yield priorPars(1 + i).last).toList
     }
   }
 
@@ -124,9 +98,9 @@ class BayesRegressor(degree: Int=1, model: String= "gaussian", priorPars: List[L
     }
 
     val intervals: Int = 3
-    val rangeB: List[List[Double]] = (meanB zip sigmaB).map{case (m, s) => List(m - intervals * s, m + intervals * s)}
-    val rangeS: List[List[Double]] = List(List(0, sigmaLike))
-    val ranges: List[List[Double]] = rangeB ::: rangeS
+    val rangeWeight: List[List[Double]] = (weightPrior zip weightPriorWidth).map{case (m, s) => List(m - intervals * s, m + intervals * s)}
+    val rangeWidth: List[List[Double]] = List(List(0, widthPrior))
+    val ranges: List[List[Double]] = rangeWeight ::: rangeWidth
     val startParams: List[Double] = ranges.map(r => Maths.mean(r))
     val params: List[Double] = maximize(0, Double.MinValue, startParams, ranges)
     ((for (i <- 0 until params.length-1) yield params(i)).toList, params.last)
@@ -135,65 +109,65 @@ class BayesRegressor(degree: Int=1, model: String= "gaussian", priorPars: List[L
   def _train(X: List[List[Double]], y: List[Double]): Unit = {
     require(X.length == y.length, "both arguments must have the same length")
     val nFeatures = X.head.length
-    initWeights(model, nFeatures)
+    initWeights(nFeatures)
     // likelihood
-    val likelihood = (b: List[Double], s: Double) => {
-      (X zip y).map{case (xi, yi) => finiteLog(Maths.normal(yi, Maths.dot(b, 1 :: xi), s))}.sum
+    val likelihood = (W: List[Double], s: Double) => {
+      (X zip y).map{case (xi, yi) => finiteLog(Maths.normal(yi, Maths.dot(W, 1 :: xi), s))}.sum
     }
     // posterior
-    val posterior = ( b: List[Double], s: Double) => {
-      likelihood(b, s) + priorB(b).map(pb => finiteLog(pb)).sum + finiteLog(priorS(s))
+    val posterior = (W: List[Double], s: Double) => {
+      likelihood(W, s) + evalWeightPrior(W).map(wp => finiteLog(wp)).sum + finiteLog(evalWidthPrior(s))
     }
     // determine maximum likelihood parameters
-    val (optimalB: List[Double], optimalS: Double) = optimize(posterior)
-    paramB = optimalB
-    paramS = optimalS
+    val (optimalWeight: List[Double], optimalWidth: Double) = optimize(posterior)
+    weight = optimalWeight
+    width = optimalWidth
 
     println("Final estimated parameter means for Y <- N(B.head + B.tail * X, S):")
-    println("B = " + paramB)
-    println("S = %.3f".format(paramS))
+    println("B = " + weight)
+    println("S = %.3f".format(width))
 
     // get equidistant points in this feature for line plotting
     val intervals = 3.0
-    val minB = min((meanB zip sigmaB).map{case (m, s) => m - intervals * s})
-    val maxB = max((meanB zip sigmaB).map{case (m, s) => m + intervals * s})
-    val minX = min(minB, 0 - intervals * sigmaLike)
-    val maxX = max(maxB, 0 + intervals * sigmaLike)
+    val minWeight = min((weightPrior zip weightPriorWidth).map{case (m, s) => m - intervals * s})
+    val maxWeight = max((weightPrior zip weightPriorWidth).map{case (m, s) => m + intervals * s})
+    val minX = min(minWeight, 0 - intervals * widthPrior)
+    val maxX = max(maxWeight, 0 + intervals * widthPrior)
     val equiVec: DenseVector[Double] = linspace(minX, maxX, 200)
     val xEqui: List[Double] = (for (i <- 0 until equiVec.size) yield equiVec(i)).toList
     val equiFeatures = List.fill(nFeatures+1)(xEqui)
     // plot some distributions
-    val valsB = equiFeatures.transpose.map(priorB(_)).transpose.map(xEqui zip _)
-    val valsS = xEqui zip (xEqui.map(priorS(_)))
-    val vals = valsB ::: List(valsS)
+    val valsWeight = equiFeatures.transpose.map(evalWeightPrior(_)).transpose.map(xEqui zip _)
+    val valsWidth = xEqui zip (xEqui.map(evalWidthPrior(_)))
+    val vals = valsWeight ::: List(valsWidth)
     val names = (for (i <- 0 to nFeatures) yield "B" + i).toList ::: List("S")
     Plotting.plotCurves(vals, names, xlabel= "Value", name= "plots/reg_Bayes_priors.pdf")
 
-    val valsPosteriorB =
+    val valsPosteriorWeight =
       (for (i <- 0 to nFeatures) yield xEqui zip (xEqui.map(eq => {
-          val eqB: List[Double] = (for (j <- 0 to nFeatures) yield if (i == j) eq else paramB(j)).toList
-          posterior(eqB, paramS)
+          val eqB: List[Double] = (for (j <- 0 to nFeatures) yield if (i == j) eq else weight(j)).toList
+          posterior(eqB, width)
         }
       ))).toList
-    val valsPosteriorS = xEqui zip (xEqui.map(eq => posterior(paramB, eq)))
-    val valsPosterior = valsPosteriorB ::: List(valsPosteriorS)
+    val valsPosteriorWidth = xEqui zip (xEqui.map(eq => posterior(weight, eq)))
+    val valsPosterior = valsPosteriorWeight ::: List(valsPosteriorWidth)
     val namesPosterior = names.map("Posterior(" + _ + ")")
     Plotting.plotCurves(valsPosterior, namesPosterior, xlabel= "Value", name= "plots/reg_Bayes_posterior_dep.pdf")
 
-    val valsLikelihoodB =
+    val valsLikelihoodWeight =
       (for (i <- 0 to nFeatures) yield xEqui zip (xEqui.map(eq => {
-          val eqB: List[Double] = (for (j <- 0 to nFeatures) yield if (i == j) eq else paramB(j)).toList
-          likelihood(eqB, paramS)
+          val eqB: List[Double] = (for (j <- 0 to nFeatures) yield if (i == j) eq else weight(j)).toList
+          likelihood(eqB, width)
         }
       ))).toList
-    val valsLikelihoodS = xEqui zip (xEqui.map(eq => likelihood(paramB, eq)))
-    val valsLikelihood = valsLikelihoodB ::: List(valsLikelihoodS)
+    val valsLikelihoodWidth = xEqui zip (xEqui.map(eq => likelihood(weight, eq)))
+    val valsLikelihood = valsLikelihoodWeight ::: List(valsLikelihoodWidth)
     val namesLikelihood = names.map("Likelihood(" + _ + ")")
     Plotting.plotCurves(valsLikelihood, namesLikelihood, xlabel= "Value", name= "plots/reg_Bayes_likelihood_dep.pdf")
   }
 
   def _predict(X: List[List[Double]]): List[Double] =
-    for (instance <- X) yield Maths.dot(paramB, 1 :: instance)
+    for (instance <- X) yield Maths.dot(weight, 1 :: instance)
 
   def predict(X: List[List[Double]]): List[Double] =
     _predict(DataTrafo.addPolyFeatures(X, degree))
