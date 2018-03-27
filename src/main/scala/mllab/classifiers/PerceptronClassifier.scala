f4a905156cb4245625f5d94af5777ce4e755c3f5
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-03-27 15:21:15 +0200

    Make more functional

--- src/main/scala/mllab/classifiers/PerceptronClassifier.scala
+++ src/main/scala/mllab/classifiers/PerceptronClassifier.scala
@@ -15,73 +15,74 @@ class PerceptronClassifier(alpha: Double = 1.0, degree: Int=1) extends Classifie
 
   val name: String = "PerceptronClassifier"
 
-  var weight = new ListBuffer[Double]()
-  var bias: Double = 0
+  var weight: List[Double] = Nil
 
   /** Calculates the distance to the decision decision hyperplane */
-  def distToPlane(instance: List[Double]): Double = {
-    val side: Double = bias + Maths.dot(weight.toList, instance)
-    side / Maths.abs(weight.toList)
+  def distToPlane(instance: List[Double], weight: List[Double]): Double = {
+    val side: Double = Maths.dot(weight, 1 :: instance)
+    side / Maths.abs(weight.tail)
   }
 
   /** Gets the prediction for this instance
     *
     * Predicts if instance is above or below the decision hyperplane
     */
-  def getPrediction(instance: List[Double]): Int =
-    if (distToPlane(instance) > 0) 1 else 0
+  def getPrediction(instance: List[Double], weight: List[Double]): Int =
+    if (distToPlane(instance, weight) > 0) 1 else 0
 
-  def isCorrect(instance: List[Double], label: Int): Boolean =
-    getPrediction(instance) == label
+  /** Determines if an instance is classified correctly */
+  def isCorrect(instance: List[Double], label: Int, weight: List[Double]): Boolean =
+    getPrediction(instance, weight) == label
 
   def _train(X: List[List[Double]], y: List[Int]): Unit = {
     require(X.length == y.length, "number of training instances and labels is not equal")
+    val nFeatures = X.head.length
 
-    // initialize parameters
-    bias  = 0
-    for (i <- 0 until X.head.length) {
-      weight += 0  // replace by Math.random?
-    }
+    def trainEpochs(count: Int, isDone: Boolean, weight: List[Double]): List[Double] = {
+      val maxEpochs: Int = 1000
+      if (count == maxEpochs || isDone) {
+          println("Final% 4d with weights ".format(count) +
+            weight.map(p => "%+.3f".format(p)).mkString(", ")
+        )
+        weight
+      }
+      else {
+        if (count % 100 == 0 || (count < 50 && count % 10 == 0) || (count < 5))
+          println("Epoch% 4d with weights ".format(count) +
+            weight.map(p => "%+.3f".format(p)).mkString(", ")
+        )
 
-    // loop over training set until all correctly classified
-    var needMoreTraining: Boolean = true
-    var epoch: Int = 0
-    val MaxEpochs: Int = 1000
-    breakable{
-      while(needMoreTraining) {
-        if (epoch > MaxEpochs) {
-          println("Maximum number of loops " + MaxEpochs + " reached! Abort training!")
-          break
-        }
-        println(epoch + ". new training loop with weights " + weight + " and bias " + bias)
-        assert (weight.length == X.head.length)
-        epoch += 1
-        needMoreTraining = false
+        var newBias = weight.head
+        var newWeightVec = weight.tail
+        var newIsDone = true
         breakable{
           for (idx <- 0 until X.length) {
             // take random instance to avoid being trapped between two too close instances
             val i: Int = (Math.random * X.length).toInt
-            if (!isCorrect(X(i), y(i))) {
-              println("Incorrect classification of instance " + i + ": " + X(i) + " true label " + y(i))
+            // println(s" - classify instance $idx: " + X(i) + " true label " + y(i))
+            if (!isCorrect(X(i), y(i), weight)) {
+              // println("Incorrect classification of instance " + i + ": " + X(i) + " true label " + y(i))
               val sign: Int = if (y(i) == 1) 1 else -1
-              bias = bias + (alpha * sign) / epoch
-              for (j <- 0 until weight.length) {
-                weight(j) = weight(j) + (alpha * sign * X(i)(j)) / epoch
-              }
-              needMoreTraining = true
+              newBias = newBias + (alpha * sign) / (count+1)
+              newWeightVec = (for (j <- 0 until newWeightVec.length) yield newWeightVec(j) + (alpha * sign * X(i)(j)) / (count+1)).toList
+              newIsDone = false
               break
             }
           }
         }
+        val newWeight = newBias :: newWeightVec
+        trainEpochs(count+1, newIsDone, newWeight)
       }
     }
-    println("Training finished after " + epoch + " epochs!")
-    println("Weight: " + weight)
-    println("Bias: " + bias)
+
+    val trainedVals = trainEpochs(0, false, 0.0 :: List.fill(nFeatures)(0.0))
+    println("New trained values: " + trainedVals)
+
+    weight = trainedVals
   }
 
   def _predict(X: List[List[Double]]): List[Int] =
-    for (instance <- X) yield getPrediction(instance)
+    for (instance <- X) yield getPrediction(instance, weight)
 
   def predict(X: List[List[Double]]): List[Int] =
     _predict(DataTrafo.addPolyFeatures(X, degree))
