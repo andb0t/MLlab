a76484dc7d1697dd5f4aa2b6b74092f788710601
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-03-27 18:25:40 +0200

    Add degree hyperparameter, remove listbuffer

--- src/main/scala/mllab/classifiers/NaiveBayesClassifier.scala
+++ src/main/scala/mllab/classifiers/NaiveBayesClassifier.scala
@@ -1,7 +1,6 @@
 package classifiers
 
-import scala.collection.mutable.ListBuffer
-
+import datastructures._
 import utils._
 
 
@@ -9,15 +8,16 @@ import utils._
  * @param model The distribution function to assume for the feature distributions
  * @param priors Prior probabilities to assume for the classes
  * @param alpha Additive (Laplace/Lidstone) smoothing parameter (0 for no smoothing)
+ * @param degree Order of polynomial features to add to the instances (1 for no addition)
  */
-class NaiveBayesClassifier(model: String="gaussian", priors: List[Double]=Nil, alpha: Double = 1.0) extends Classifier {
+class NaiveBayesClassifier(model: String="gaussian", priors: List[Double]=Nil, alpha: Double = 1.0, degree: Int=2) extends Classifier {
 
   val name: String = "NaiveBayesClassifier"
 
   /** Prior probability for the classes */
-  var prior = new ListBuffer[Double]()
+  var prior: List[Double] = Nil
   /** Parameters for the likelihood for each class and feature */
-  var params = new ListBuffer[List[List[Double]]]()
+  var params: List[List[List[Double]]] = Nil
 
   if (priors != Nil)
     assert (priors.sum == 1, "provided priors do not add up to unity")
@@ -25,13 +25,13 @@ class NaiveBayesClassifier(model: String="gaussian", priors: List[Double]=Nil, a
   /** Calculates the per class and per feature likelihood value of a given instance */
   def getLikeli(x: List[Double]): List[List[Double]] =
     if (model == "gaussian")
-      (for (pClass <- params) yield for (fp <- x zip pClass) yield Maths.normal(fp._1, fp._2.head, fp._2(1))).toList
+      for (pClass <- params) yield for (fp <- x zip pClass) yield Maths.normal(fp._1, fp._2.head, fp._2(1))
     else if (model == "triangular")
-      (for (pClass <- params) yield for (fp <- x zip pClass) yield Maths.triangular(fp._1, fp._2.head, fp._2(1))).toList
+      for (pClass <- params) yield for (fp <- x zip pClass) yield Maths.triangular(fp._1, fp._2.head, fp._2(1))
     else if (model == "rectangular")
-      (for (pClass <- params) yield for (fp <- x zip pClass) yield Maths.rectangular(fp._1, fp._2.head, fp._2(1))).toList
+      for (pClass <- params) yield for (fp <- x zip pClass) yield Maths.rectangular(fp._1, fp._2.head, fp._2(1))
     else if (model == "bernoulli")
-      (for (pClass <- params) yield for (fp <- x zip pClass) yield Maths.bernoulli(fp._1.toInt, fp._2.head)).toList
+      for (pClass <- params) yield for (fp <- x zip pClass) yield Maths.bernoulli(fp._1.toInt, fp._2.head)
     else
       Nil
 
@@ -43,28 +43,25 @@ class NaiveBayesClassifier(model: String="gaussian", priors: List[Double]=Nil, a
    */
   def getProbabs(x: List[Double]): List[Double] =
     if (model == "multinomial")
-      (for (prpa <- prior zip params.map(_.flatten)) yield prpa._1 * Maths.multinomial(x.map(_.toInt), prpa._2)).toList
+      for (prpa <- prior zip params.map(_.flatten)) yield prpa._1 * Maths.multinomial(x.map(_.toInt), prpa._2)
     else
-      (for (pl <- prior zip getLikeli(x)) yield pl._1 * pl._2.product).toList
+      for (pl <- prior zip getLikeli(x)) yield pl._1 * pl._2.product
 
-
-  def train(X: List[List[Double]], y: List[Int]): Unit = {
+  def _train(X: List[List[Double]], y: List[Int]): Unit = {
     require(X.length == y.length, "number of training instances and labels is not equal")
     val classes: List[Int] = y.toSet.toList.sorted
     if (priors != Nil) {
-      for (p <- priors) prior += p
+      prior = priors
       assert (prior.length == classes.length, "number of provided priors does not match number of classes")
     }
     else {
       println("Determine priors from training set frequencies")
-      for (cl <- classes) {
-        prior += 1.0 * y.count(_==cl) / y.length
-      }
+      prior = for (cl <- classes) yield 1.0 * y.count(_==cl) / y.length
     }
     println("Prior:")
     println(prior.zipWithIndex.map{case (p, c) => " - class " + c + ": " + p}.mkString("\n"))
     println(s"Determine $model model parameters from training features")
-    for (cl <- classes) {
+    params = for (cl <- classes) yield {
       val thisClassX = (X zip y).filter(_._2 == cl).map(_._1)
       val thisClassFeatures = thisClassX.transpose
       val featParams: List[List[Double]] = for (feature <- thisClassFeatures) yield {
@@ -80,7 +77,7 @@ class NaiveBayesClassifier(model: String="gaussian", priors: List[Double]=Nil, a
             List((feature.sum + alpha) / (thisClassFeatures.flatten.sum + thisClassFeatures.length * alpha))
         else throw new NotImplementedError("Bayesian model " + model + " not implemented")
       }
-      params += featParams
+      featParams
     }
     println("%s model parameters: ".format(model.capitalize))
     for (cp <- classes zip params) {
@@ -90,6 +87,13 @@ class NaiveBayesClassifier(model: String="gaussian", priors: List[Double]=Nil, a
     }
   }
 
-  def predict(X: List[List[Double]]): List[Int] =
+  def _predict(X: List[List[Double]]): List[Int] =
     for (instance <- X) yield getProbabs(instance).zipWithIndex.maxBy(_._1)._2
+
+    def predict(X: List[List[Double]]): List[Int] =
+      _predict(DataTrafo.addPolyFeatures(X, degree))
+
+    def train(X: List[List[Double]], y: List[Int]): Unit =
+      _train(DataTrafo.addPolyFeatures(X, degree), y)
+
 }
