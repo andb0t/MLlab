1d4f08657d696802e031d28e2aa97c67616c9469
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-03-27 13:56:10 +0200

    Add zoom to optimizer

--- src/main/scala/mllab/datastructures/Optimizer.scala
+++ src/main/scala/mllab/datastructures/Optimizer.scala
@@ -6,34 +6,46 @@ import utils._
 /** Provides functions for optimizing functions */
 object Optimizer {
   /** Determines the function maximum by random walks */
-  def optimize(func: (List[Double]) => Double, startParams: List[Double], startRanges: List[List[Double]]): List[Double] = {
+  def optimize(func: (List[Double]) => Double, startParams: List[Double], startRanges: List[List[Double]], silent: Boolean=false): List[Double] = {
 
-    def maximize(count: Int, maximum: Double, params: List[Double], ranges: List[List[Double]]): List[Double] = {
-      val nSteps = 1000
+    def maximize(count: Int, zooms: Int, maximum: Double, params: List[Double], ranges: List[List[Double]]): List[Double] = {
+      val nSteps = params.length * 200
       val numberDimensions: Int = ranges.length
       if (count == nSteps) {
-        println(s"- final step $count: optimum %.3f, params ".format(maximum) +
-        "(" + params.map(p => Maths.round(p, 3)).mkString(", ") + ")"
-        )
-        params
+        if (!silent)
+          println(s"- final optim. step $count: optimum %.3f, params ".format(maximum) +
+            "(" + params.map(p => Maths.round(p, 3)).mkString(", ") + ")"
+          )
+        if (zooms == 1) params
+        else {
+          val zoomFactor = 0.25
+          val newWindow: List[Double] = ranges.map(range => zoomFactor * (range(1) - range.head))
+          val newRanges: List[List[Double]] = (params zip newWindow).map{case (p, s) => List(p - s / 2, p + s / 2)}
+          maximize(0, zooms-1, maximum, params, newRanges)
+        }
       }
       else {
-        if (count % 100 == 0 || (count < 50 && count % 10 == 0) || (count < 5))
+        if (!silent && count == 0) {
+          println("Optimize within these parameter ranges")
+          println((ranges.zipWithIndex.map(ri => "- parameter %d: %.3f - %.3f".format(ri._2, ri._1.head, ri._1(1)))).mkString("\n"))
+          println("Optimization:")
+       }
+        if (!silent && (count % 100 == 0 || (count < 50 && count % 10 == 0) || (count < 5)))
           println(s"- optimization step $count: optimum %.3e, params ".format(maximum) +
-          "(" + params.map(p => Maths.round(p, 3)).mkString(", ") + ")"
+            "(" + params.map(p => Maths.round(p, 3)).mkString(", ") + ")"
         )
         val dimension: Int = scala.util.Random.nextInt(numberDimensions)
         val sign: Int = scala.util.Random.nextInt(2) * 2 - 1
-        val step: Double = 1.0 * sign * (ranges(dimension)(1) - ranges(dimension).head) / 100
+        val step: Double = 1.0 * sign * (ranges(dimension)(1) - ranges(dimension).head) / 10
         // println(s"Step $count: step %.3f in dimension $dimension".format(step))
         val newParams: List[Double] = params.zipWithIndex.map{case (p, i) => if (i == dimension) p + step else p}
         val newMaximum: Double = func(newParams)
         // if (newMaximum > maximum) println("New maximum " + maximum + " at " + params)
-        if (newMaximum > maximum) maximize(count+1, newMaximum, newParams, ranges)
-        else maximize(count+1, maximum, params, ranges)
+        if (newMaximum > maximum) maximize(count+1, zooms, newMaximum, newParams, ranges)
+        else maximize(count+1, zooms, maximum, params, ranges)
       }
     }
 
-    maximize(0, Double.MinValue, startParams, startRanges)
+    maximize(0, 3, Double.MinValue, startParams, startRanges)
   }
 }
