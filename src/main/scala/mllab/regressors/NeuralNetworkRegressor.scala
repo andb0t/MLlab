443f465107d95667269da2dda548e592ae975659
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-03-30 13:49:04 +0200

    Set up predict method

--- src/main/scala/mllab/regressors/NeuralNetworkRegressor.scala
+++ src/main/scala/mllab/regressors/NeuralNetworkRegressor.scala
@@ -1,17 +1,36 @@
 package regressors
 
+import scala.collection.mutable.ListBuffer
+
+import breeze.linalg._
+
+import utils._
+import datastructures._
+
 
 /** Neural network regressor
  *
  */
-class NeuralNetworkRegressor() extends Regressor {
+class NeuralNetworkRegressor(
+    activation: String = "tanh",
+    layers: List[Int] = List(1, 4, 1)
+) extends Regressor {
+
+  require(layers.length > 2, "too few layers: need at least an input, a middle and an output layer")
 
   val name: String = "NeuralNetworkRegressor"
 
-  def train(X: List[List[Double]], y: List[Double]): Unit =
-    require(X.length == y.length, "both arguments must have the same length")
+  val W: IndexedSeq[DenseMatrix[Double]] = for (i <- 0 until layers.length - 1) yield DenseMatrix.rand[Double](layers(i),layers(i+1))
+  val b: IndexedSeq[DenseVector[Double]] = for (i <- 0 until layers.length - 1) yield DenseVector.zeros[Double](layers(i+1))
 
-  def predict(X: List[List[Double]]): List[Double] =
-    for (instance <- X) yield Math.random
+  def train(listX: List[List[Double]], listy: List[Double]): Unit =
+    require(listX.length == listy.length, "both arguments must have the same length")
+
+  def predict(listX: List[List[Double]]): List[Double] = {
+    val X: DenseMatrix[Double] = Trafo.toMatrix(listX)
+    val output = NeuralNetwork.feedForward(X, W, b, activation)
+    val prediction: DenseVector[Double] = output(::, 0)
+    (for (i <- 0 until prediction.size) yield prediction(i)).toList
+  }
 
 }
