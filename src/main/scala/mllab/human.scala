c53b2241573848cad27fe09f0daada20849085fb
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-03-31 13:17:35 +0200

    Finish command line parsing

--- src/main/scala/mllab/human.scala
+++ src/main/scala/mllab/human.scala
@@ -3,7 +3,7 @@ package human
 import play.api.libs.json.{JsValue, Json}
 
 object Human {
-  case class FuncArguments(animal: String, color: String, count: Int)
+  case class FuncArguments(animal: String, color: String, count: String)
   implicit val funcArgumentsFormat = Json.format[FuncArguments]
   implicit def jsValueToFuncArguments(json: JsValue): FuncArguments = json.as[FuncArguments]
   def human2(json: JsValue): Human = {
@@ -11,13 +11,13 @@ object Human {
   }
 }
 
-case class Human(animal: String, color: String, count: Int=2) {
-  println(s"Human says: 'Oh, that's $color $animal number $count!'")
+case class Human(animal: String, color: String, count: String) {
+  println(s"Initialize human who saw the same $color $animal $count times'")
 
   def ask(): Unit =
-    println("I saw %d %ss".format(count, animal))
+    println("'I saw %s different %s %ss!'".format(count, color, animal))
 
   override def toString(): String =
-    "A human who saw %d %ss".format(count, animal)
+    "A human who thinks he saw %s different %s %ss".format(count, color, animal)
 
 }
--- src/main/scala/mllab/test.scala
+++ src/main/scala/mllab/test.scala
@@ -7,10 +7,22 @@ import human._
 object CatEncounter {
   class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
     val hyper = opt[String](
-      default = Some("animal=cat, color=black"),
+      default = Some("animal=cat, color=black, count=2"),
       descr = "hyperparameters to pass to the algorithm"
     )
     verify()
+  }
+
+  def jsonify(hyper: String): JsValue = {
+    val args = hyper.split(',').map(text => text.split("=").map(_.trim))
+    val argsMap: Map[String, String] = args.map(a => (a(0) -> a(1))).toMap
+    Json.toJson(argsMap)
+  }
+
+  def simpleJson(): JsValue = {
+    // val json = Json.obj("color" -> "black", "animal" -> "cat")
+    val json = Json.obj("color" -> "black", "animal" -> "cat", "count" -> "2")
+    json
   }
 
   def main(args: Array[String]): Unit = {
@@ -18,8 +30,8 @@ object CatEncounter {
     if (!conf.hyper().isEmpty())
       println("Chosen hyperparameters: " + conf.hyper)
 
-    val json = Json.obj("color" -> "black", "animal" -> "cat", "count" -> 2)
-    // val json = Json.obj("color" -> "black", "animal" -> "cat")
+    // val json = simpleJson()
+    val json = jsonify(conf.hyper())
     val hum = Human.human2(json)
     println(hum)
     hum.ask()
