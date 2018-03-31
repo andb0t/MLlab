fb250b61599612fc751268f050dbccc84640925a
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-03-31 14:38:20 +0200

    Fix ordering issue

--- src/main/scala/mllab/human.scala
+++ src/main/scala/mllab/human.scala
@@ -4,12 +4,14 @@ import play.api.libs.json.JsValue
 
 import json._
 
-
-case class Human(animal: String, color: String, count: String="0") {
+/**
+ * @todo pick up default arguments from main constructor
+ */
+case class Human(color: String, animal: String, count: String="0") {
   def this(json: JsValue) = {
-    this(JsonMagic.stringify(json, "animal", "object"),
-         JsonMagic.stringify(json, "color", "unvisible"),
-         JsonMagic.stringify(json, "count", "0"))
+    this(animal=JsonMagic.stringify(json, "animal", "object"),
+         color=JsonMagic.stringify(json, "color", "invisible"),
+         count=JsonMagic.stringify(json, "count", "0"))
   }
 
   println(s"Initialize human who saw the same $color $animal $count times'")
