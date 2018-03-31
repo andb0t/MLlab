9c7440f2edc9323ec8d9788222149e090f23df9c
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-03-31 17:06:51 +0200

    Add documentation

--- src/main/scala/mllab/utils/Json.scala
+++ src/main/scala/mllab/utils/Json.scala
@@ -3,7 +3,10 @@ package json
 import play.api.libs.json.{JsValue, Json, JsNull}
 
 
+/** Provides JSON conversion functions */
 object JsonMagic {
+
+  /** Gets a String from a JSON object */
   def toString(json: JsValue, key: String, default: String): String =
     try
       json(key).toString().replaceAll("^\"|\"$", "")
@@ -13,6 +16,7 @@ object JsonMagic {
       case _: java.lang.Exception => default
     }
 
+  /** Gets an Int from a JSON object */
   def toInt(json: JsValue, key: String, default: Int): Int =
     try
       json(key).toString().toInt
@@ -22,6 +26,7 @@ object JsonMagic {
       case _: java.lang.Exception => default
     }
 
+  /** Gets a Double from a JSON object */
   def toDouble(json: JsValue, key: String, default: Double): Double =
     try
       json(key).toString().toDouble
@@ -31,6 +36,7 @@ object JsonMagic {
       case _: java.lang.Exception => default
     }
 
+  /** Gets a List[Int] from a JSON object */
   def toListInt(json: JsValue, key: String, default: List[Int]): List[Int] =
     try
       json(key).toString().split(',').map(_.replaceAll("[^0-9]", "").toInt).toList
@@ -40,6 +46,7 @@ object JsonMagic {
       case _: java.lang.Exception => default
     }
 
+  /** Parses a string into argument value pairs */
   def jsonify(str: String, verbose: Boolean= true): JsValue = {
     val regex = """,(?![^\(\[]*[\]\)])"""
     val args = str.split(regex).map(text => text.split("=").map(_.trim))
