927caa1e7019590fc80764771c85bffaae2e5aea
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-05-29 13:31:30 +0200

    Fix tests

--- src/test/scala/mllab/ClassifierSuite.scala
+++ src/test/scala/mllab/ClassifierSuite.scala
@@ -20,10 +20,11 @@ class ClassifierSuite extends FunSuite {
   test ("DecisionTree: getPurity") {
     val featureX = List(1.0, 2.0, 3.0, 3.0, 2.5)
     val yThisNode = List(0, 0, 1, 1, 0)
+    val weightsThisNode = List(1.0, 1.0, 1.0, 1.0, 1.0)
     def roundTupleOne(x: Tuple2[Double, Boolean]): Tuple2[Double, Boolean] = Tuple2(Maths.round(x._1, 9), x._2)
-    assert (new DecisionTreeClassifier().getPurity(featureX, yThisNode, 2.7, "gini") === (0, true))
-    assert (roundTupleOne(new DecisionTreeClassifier().getPurity(featureX, yThisNode, 2.2, "gini")) === (Maths.round(-4.0/15, 9), true))
-    assert (new DecisionTreeClassifier().getPurity(featureX, yThisNode, 0, "gini") === (-0.48, true))
+    assert (new DecisionTreeClassifier().getPurity(featureX, yThisNode, weightsThisNode, 2.7, "gini") === (0, true))
+    assert (roundTupleOne(new DecisionTreeClassifier().getPurity(featureX, yThisNode, weightsThisNode, 2.2, "gini")) === (Maths.round(-4.0/15, 9), true))
+    assert (new DecisionTreeClassifier().getPurity(featureX, yThisNode,weightsThisNode, 0, "gini") === (-0.48, true))
   }
 
 }
