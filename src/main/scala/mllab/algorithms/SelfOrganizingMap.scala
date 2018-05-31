75d864c39684729dfcb536eb7637172b7ad49d8e
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-05-31 12:11:56 +0200

    Finish classifier

--- src/main/scala/mllab/algorithms/SelfOrganizingMap.scala
+++ src/main/scala/mllab/algorithms/SelfOrganizingMap.scala
@@ -9,23 +9,36 @@ class SelfOrganizingMap(height: Int, width: Int) {
 
   var nodes: List[DenseVector[Double]] = Nil
 
-  println("Initializing SOM: " + this)
-
   def initialize(X: List[List[Double]]): Unit = {
     val nFeatures = X.head.length
-    nodes = List.fill(height * width)(DenseVector.fill(nFeatures){scala.util.Random.nextDouble - 0.5})
+    val features = X.transpose
+    val mins = features.map(_.min)
+    val maxs = features.map(_.max)
+    println("Initializing SOM:")
+    for (i <- 0 until nFeatures)
+      println("Feature %d range: %.3f - %.3f".format(i, mins(i), maxs(i)))
+    nodes = (for (i <- 0 until height * width) yield
+      DenseVector.tabulate(nFeatures){i => mins(i) + scala.util.Random.nextDouble * (maxs(i) - mins(i))}
+    ).toList
+    println("Initial nodes:")
+    println(nodes.mkString("\n"))
+    println("Initialized SOM: " + this)
   }
 
   def update(x: List[Double]): Unit = {
+    println("Updated nodes:")
+    println(nodes.mkString("\n"))
   }
 
-  def classifiy(x: List[Double]): Int =
-    0
+  def classifiy(x: List[Double]): Int = {
+    val distances: List[Double] = nodes.map{n => norm(n - Trafo.toVector(x))}
+    distances.zipWithIndex.minBy(_._1)._2
+  }
 
   def getMap(): List[List[Double]] =
     nodes.map(Trafo.toList(_))
 
   override def toString(): String = {
-    "SOM(%d x %d)".format(height, width)
+    "SOM(%d x %d x %d)".format(height, width, nodes.head.length)
   }
 }
