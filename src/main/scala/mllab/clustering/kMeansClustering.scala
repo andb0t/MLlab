08a0545bd7b2ee8800437c86c7db5942d8214b07
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-05-31 08:08:56 +0200

    Externalize clustering

--- src/main/scala/mllab/clustering/kMeansClustering.scala
+++ src/main/scala/mllab/clustering/kMeansClustering.scala
@@ -34,6 +34,32 @@ class kMeansClustering(
   def clusterMeans(): List[List[List[Double]]] =
     centroidEvolution.toList.transpose
 
+  def clusterToCentroid(count: Int, X: List[List[Double]], y: List[Int], centroids: List[List[Double]], stop: Boolean, maxIter: Int): Tuple2[List[Int], List[List[Double]]] = {
+    if (count >= maxIter || stop) {
+      val loss = kMeans.getLoss(X, y, centroids)
+      lossEvolution += Tuple2(count.toDouble, loss)
+      centroidEvolution += centroids
+      println("Final% 4d with loss %.4e".format(count, loss))
+      if (stop) println("Stable means reached!")
+      else if (count >= maxIter) println(s"Maximum number of iterations ($maxIter) reached!")
+      Tuple2(y, centroids)
+    }
+    else {
+      val newy: List[Int] = X.map(x => (for (centroid <- centroids) yield Maths.distance(centroid, x)).zipWithIndex.min._2)
+      if (count % 100 == 0 || (count < 50 && count % 10 == 0) || (count < 5)) {
+        val loss = kMeans.getLoss(X, newy, centroids)
+        lossEvolution += Tuple2(count.toDouble, loss)
+        println("Step% 5d with loss %.4e".format(count, loss))
+      }
+      centroidEvolution += centroids
+      val newCentroids: List[List[Double]] = kMeans.getCentroids(X, newy, k)
+      if (newCentroids.toSet != centroids.toSet)
+      clusterToCentroid(count+1, X, newy, newCentroids, stop, maxIter)
+      else
+      clusterToCentroid(count+1, X, newy, centroids, true, maxIter)
+    }
+  }
+
   def train(X: List[List[Double]]): Unit =
     println(s"No training necessary for $name")
 
@@ -42,33 +68,7 @@ class kMeansClustering(
     val range = 2
     val centroids: List[List[Double]] = List.fill(k)(List.fill(nFeatures)((scala.util.Random.nextDouble - 0.5) * range))
     val maxIter = 100
-
-    def clusterToCentroid(count: Int, X: List[List[Double]], y: List[Int], centroids: List[List[Double]], stop: Boolean): Tuple2[List[Int], List[List[Double]]] = {
-      if (count >= maxIter || stop) {
-        val loss = kMeans.getLoss(X, y, centroids)
-        lossEvolution += Tuple2(count.toDouble, loss)
-        centroidEvolution += centroids
-        println("Final% 4d with loss %.4e".format(count, loss))
-        if (stop) println("Stable means reached!")
-        else if (count >= maxIter) println(s"Maximum number of iterations ($maxIter) reached!")
-        Tuple2(y, centroids)
-      }
-      else {
-        val newy: List[Int] = X.map(x => (for (centroid <- centroids) yield Maths.distance(centroid, x)).zipWithIndex.min._2)
-        if (count % 100 == 0 || (count < 50 && count % 10 == 0) || (count < 5)) {
-          val loss = kMeans.getLoss(X, newy, centroids)
-          lossEvolution += Tuple2(count.toDouble, loss)
-          println("Step% 5d with loss %.4e".format(count, loss))
-        }
-        centroidEvolution += centroids
-        val newCentroids: List[List[Double]] = kMeans.getCentroids(X, newy, k)
-        if (newCentroids.toSet != centroids.toSet)
-          clusterToCentroid(count+1, X, newy, newCentroids, stop)
-        else
-          clusterToCentroid(count+1, X, newy, centroids, true)
-      }
-    }
-    clusterToCentroid(0, X, Nil, centroids, false)._1
+    clusterToCentroid(0, X, Nil, centroids, false, maxIter)._1
   }
 
   override def diagnostics(): Map[String, List[(Double, Double)]] = {
