c8bf7b0db481c8cc0d6799a70992b4ec06e32295
Andreas Alexander Maier <andreas.alexander.maier@cern.ch>
2018-06-01 08:53:25 +0200

    Add iterations hyperparameter

--- src/main/scala/mllab/clustering/SelfOrganizingMapClustering.scala
+++ src/main/scala/mllab/clustering/SelfOrganizingMapClustering.scala
@@ -14,24 +14,33 @@ object SelfOrganizingMapClustering {
   val width: Int = 3
   val height: Int = 5
   val alpha: Double = 0.2
+  val lambda: Int = -1
 }
 
 /** Self-organizing map clustering
+ @param width width (in number of nodes) of the SOM
+ @param height height (in number of nodes) of the SOM
+ @param alpha size of the update step. Ranges from 0 (no update) to 1 (set equal to training instance)
+ @param lambda number of iterations for random training. Default -1: run once and in order through training data
  */
  class SelfOrganizingMapClustering(
    width: Int = SelfOrganizingMapClustering.width,
    height: Int = SelfOrganizingMapClustering.height,
-   alpha: Double = SelfOrganizingMapClustering.alpha
+   alpha: Double = SelfOrganizingMapClustering.alpha,
+   lambda: Int = SelfOrganizingMapClustering.lambda
  ) extends Clustering {
    def this(json: JsValue) = {
      this(
        width = JsonMagic.toInt(json, "width", SelfOrganizingMapClustering.width),
        height = JsonMagic.toInt(json, "height", SelfOrganizingMapClustering.height),
-       alpha = JsonMagic.toDouble(json, "alpha", SelfOrganizingMapClustering.alpha)
+       alpha = JsonMagic.toDouble(json, "alpha", SelfOrganizingMapClustering.alpha),
+       lambda = JsonMagic.toInt(json, "lambda", SelfOrganizingMapClustering.lambda)
        )
    }
 
    val name: String = "SelfOrganizingMapClustering"
+
+   require(alpha >= 0 && alpha <= 1, "hyper parameter alpha has to be in interval [0, 1]")
 
    val SOM = new SelfOrganizingMap(height, width, alpha)
 
@@ -40,7 +49,12 @@ object SelfOrganizingMapClustering {
 
    def train(X: List[List[Double]]): Unit = {
      SOM.initialize(X)
-     for (instance <- X) SOM.update(instance)
+     if (lambda == -1) {
+       for (instance <- X) SOM.update(instance)
+     }else{
+       val nInstances = X.length
+       for (_ <- 0 until lambda) SOM.update(X(scala.util.Random.nextInt(nInstances)))
+     }
    }
 
    def predict(X: List[List[Double]]): List[Int] =
